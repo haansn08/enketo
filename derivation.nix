69878e029f94a7f2a7bb914405093ca2ed535b4e
Stefan Haan <stefan.haan@aau.at>
2026-05-14 07:23:21 +0200

    derivation second attempt

--- derivation.nix
+++ derivation.nix
@@ -1,15 +1,35 @@
-{lib, stdenv, fetchYarnDeps, yarnConfigHook, yarnBuildHook, yarnInstallHook, nodejs_20}:
-stdenv.mkDerivation {
+{lib, stdenv, fetchzip, fetchurl, fetchYarnDeps, yarnConfigHook, yarnBuildHook, yarnInstallHook, nodejs, python3, python3Packages, nodePackages, sass}:
+stdenv.mkDerivation (finalAttrs: {
 	pname = "enketo";
 	version = "7.6.1";
-	src = ./.;
-	yarnOfflineCache = fetchYarnDeps {
-		yarnLock = ./yarn.lock;
-		hash = "sha256-tT8by1NYznfVSETKYJRdtGeBX98DYPkf4eaXZOptr20=";
+	src = fetchzip {
+		url = "https://github.com/enketo/enketo/archive/refs/tags/${finalAttrs.version}.tar.gz";
+		hash = "sha256-qls3SYrz7JqIAdUHGojny6gedt5CIaMie918cw4tPio=";
 	};
+	yarnOfflineCache = fetchYarnDeps {
+		yarnLock = finalAttrs.src + "/yarn.lock";
+		hash = "sha256-XEzgF30p1PkejdRBXIucWxRwrMReHyuazgyaxR6bpk4=";
+	};
+	yarnBuildScript = "install";
+	yarnBuildFlags = "--offline --frozen-lockfile";
+	postBuildHook = "yarn cache clean";
+	
+	PUPPETEER_SKIP_DOWNLOAD = "1";
+	#SKIP_SASS_BINARY_DOWNLOAD_FOR_CI = "1";
+	SASS_BINARY_NAME = "${sass}/bin/sass";
+	#npm_package_config_node_gyp_tarball = fetchurl {
+	#	url = "https://nodejs.org/download/release/v22.22.2/node-v22.22.2-headers.tar.gz";
+	#	hash = "sha256-kOXvD98C+ISH+QSnmINrNb1EiWBG1QKHO8YlrCuu3tI=";
+	#};
+	#node_gyp_silly = "1";
+	
 	nativeBuildInputs = [
 		yarnConfigHook
 		yarnBuildHook
-		nodejs_20
+		yarnInstallHook
+		nodejs
+		python3
+		python3Packages.distutils
+		sass
 	];
-}
+})
